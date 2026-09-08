3e101be673cd2dfa2a80261e60fbab17c20ea080
Кирилл Соколов <sokol4ru@MacBook-Air-Kirill.local>
2026-09-08 17:32:52 +0300

    Исправлен экспорт auth и db

--- static/js/firebase-config.js
+++ static/js/firebase-config.js
@@ -1,3 +1,7 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCm34fJuGsFXUPSk8eHlBECDDajq9ImaOs",
   authDomain: "parallel-programming-site.firebaseapp.com",
@@ -6,3 +10,8 @@ const firebaseConfig = {
   messagingSenderId: "1050372244196",
   appId: "1:1050372244196:web:2b384a48d2cd05b5b3e2f1"
 };
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const API_KEY = firebaseConfig.apiKey;
