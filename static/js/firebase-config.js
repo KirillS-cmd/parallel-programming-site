ace03a1664bc884e5b372d121d94409dd91da9e5
Кирилл Соколов <sokol4ru@MacBook-Air-Kirill.local>
2026-09-08 17:43:43 +0300

    Исправлен экспорт auth и db

--- static/js/firebase-config.js
+++ static/js/firebase-config.js
@@ -12,6 +12,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const API_KEY = firebaseConfig.apiKey;
+export const auth = getAuth(app);        // ← обязательно
+export const db = getFirestore(app);     // ← обязательно
+export const API_KEY = firebaseConfig.apiKey; // ← обязательно
